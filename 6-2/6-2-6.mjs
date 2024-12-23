0c6f0afcfe816b99e21ea43b75d5c698b9a545a5
hc421007 <nakshc421007@g.neec.ac.jp>
2024-12-23 13:17:48 +0900

    6-2-6

---
+++ 6-2/6-2-6.mjs
@@ -0,0 +1,8 @@
+const arrayTexts = [
+    "A",
+    "Test",
+    "Array"
+    ]
+    console.log(arrayTexts[0],arrayTexts[1],arrayTexts[2],)
+    arrayTexts[1] = "NewText"
+    console.log(arrayTexts[0],arrayTexts[1],arrayTexts[2],)
