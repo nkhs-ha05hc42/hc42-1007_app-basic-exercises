97784ccefdfcabffa6c29dada3714157c23de64e
hc421007 <nakshc421007@g.neec.ac.jp>
2024-12-23 14:40:32 +0900

    6-5-2

---
+++ 6-5/6-5-2.mjs
@@ -0,0 +1,11 @@
+for (let i = 1; i <=100; i++)
+    {
+        if(i<=59)
+        {
+            if(i>=50)
+            {
+                continue
+            }
+        }
+        console.log(i)
+    }
