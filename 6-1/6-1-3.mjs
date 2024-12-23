669f112e057b2700563f8cd8bff5408cb05909f5
hc421007 <nakshc421007@g.neec.ac.jp>
2024-12-23 11:54:46 +0900

    6-1-3

---
+++ 6-1/6-1-3.mjs
@@ -0,0 +1,9 @@
+const testObject = {
+    a: 10,
+    b: 20,
+}
+console.log(testObject.a+testObject.b)
+console.log(testObject.a-testObject.b)
+console.log(testObject.a*testObject.b)
+console.log(testObject.a/testObject.b)
+console.log(testObject.a%testObject.b)
