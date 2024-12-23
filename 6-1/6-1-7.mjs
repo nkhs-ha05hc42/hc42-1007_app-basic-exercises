f5a042a738da559d75716fd29a72751b0dcd27c4
hc421007 <nakshc421007@g.neec.ac.jp>
2024-12-23 11:55:35 +0900

    6-1-7

---
+++ 6-1/6-1-7.mjs
@@ -0,0 +1,10 @@
+const testObject = {
+    a: 10,
+    b: 20,
+    c: "10",
+    d: 10,
+}
+
+console.log(testObject.a === testObject.b)
+console.log(testObject.a === testObject.c)
+console.log(testObject.a === testObject.d)
