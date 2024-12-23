6371f750bec6410d5c11310c97d8ce677c6743b2
hc421007 <nakshc421007@g.neec.ac.jp>
2024-12-23 11:55:24 +0900

    6-1-6

---
+++ 6-1/6-1-6.mjs
@@ -0,0 +1,50 @@
+const testObject = {
+    a: 10,
+    b1: 1,
+    b2: 4,
+    b3: 9,
+    b4: 10,
+    b5: 11,
+    b6: 15,
+    b7: 20,
+    b8: 22,
+}
+console.log(testObject.a > testObject.b1)
+console.log(testObject.a >= testObject.b1)
+console.log(testObject.a < testObject.b1)
+console.log(testObject.a <= testObject.b1)
+
+console.log(testObject.a > testObject.b2)
+console.log(testObject.a >= testObject.b2)
+console.log(testObject.a < testObject.b2)
+console.log(testObject.a <= testObject.b2)
+
+console.log(testObject.a > testObject.b3)
+console.log(testObject.a >= testObject.b3)
+console.log(testObject.a < testObject.b3)
+console.log(testObject.a <= testObject.b3)
+
+console.log(testObject.a > testObject.b4)
+console.log(testObject.a >= testObject.b4)
+console.log(testObject.a < testObject.b4)
+console.log(testObject.a <= testObject.b4)
+
+console.log(testObject.a > testObject.b5)
+console.log(testObject.a >= testObject.b5)
+console.log(testObject.a < testObject.b5)
+console.log(testObject.a <= testObject.b5)
+
+console.log(testObject.a > testObject.b6)
+console.log(testObject.a >= testObject.b6)
+console.log(testObject.a < testObject.b6)
+console.log(testObject.a <= testObject.b6)
+
+console.log(testObject.a > testObject.b7)
+console.log(testObject.a >= testObject.b7)
+console.log(testObject.a < testObject.b7)
+console.log(testObject.a <= testObject.b7)
+
+console.log(testObject.a > testObject.b8)
+console.log(testObject.a >= testObject.b8)
+console.log(testObject.a < testObject.b8)
+console.log(testObject.a <= testObject.b8)
