684af74345924b4c33b1407efdc4009627221c1a
hc421007 <nakshc421007@g.neec.ac.jp>
2024-12-23 14:08:36 +0900

    6-4-3

---
+++ 6-4/6-4-3.mjs
@@ -0,0 +1,17 @@
+const a = 50
+if (a>=90)
+{
+    console.log("Excellent")
+}
+else if (a>=80)
+{
+    console.log("good")
+}
+else if(a>=60)
+{
+    console.log("Normal")
+}
+else
+{
+    console.log("Red")
+}
